dfb36b4ce246e999f21f5402ec188d463855fc85
pannagaJois <194781615+pannagaJois@users.noreply.github.com>
2025-10-12 23:02:49 +0530

    selection sort

---
+++ s2/lec1/selection_sort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void sort(int a[], int n) {
+  for (int i = 0; i < n-1; i++) {
+    int min_index = i;
+    for(int j = i+1; j < n; j++)
+      if(a[min_index] > a[j])
+        min_index = j;
+    std::swap(a[min_index], a[i]);
+  }
+}
+int main (int argc, char *argv[]) {
+  int a[] = {5, 3, 6, 2, 1, 4};
+
+  sort(a, 6);
+
+  for (int i = 0; i < 6; i++)
+    std::cout << a[i] << "  ";
+  std::cout << std::endl;
+  return 0;
+}
