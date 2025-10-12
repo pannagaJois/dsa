e9df0a9dc220f971b999d083660e7d376bcacfb9
pannagaJois <194781615+pannagaJois@users.noreply.github.com>
2025-10-12 19:58:52 +0530

    hash 5 - using maps, for string

---
+++ s1/lec6/hash5.cpp
@@ -0,0 +1,20 @@
+// hashing using maps - for chars
+
+#include<iostream>
+#include<map>
+#include<string>
+
+int main (int argc, char *argv[]) {
+  std::cout << "enter a string: ";
+  std::string s;
+  std::getline(std::cin, s);
+
+  std::map<char, int> m;
+
+  for (int i = 0; i < s.length(); i++)
+    m[s[i]]++;
+
+  for(auto it : m)
+    std::cout << it.first << " : " << it.second << std::endl;
+  return 0;
+}
