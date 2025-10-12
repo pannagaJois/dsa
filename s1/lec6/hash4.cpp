5f3d634d157be52bd6e674dea38cea1252be7c23
pannagaJois <194781615+pannagaJois@users.noreply.github.com>
2025-10-12 19:48:40 +0530

    hash 4 - using maps, for ints

---
+++ s1/lec6/hash4.cpp
@@ -0,0 +1,21 @@
+// hashing using maps - integer values
+#include<iostream>
+#include<map>
+
+int main (int argc, char *argv[]) {
+  int n;
+  std::cout << "enter the no of elements: ";
+  std::cin >> n;
+  int a[n];
+  std::cout << "enter the elements: ";
+  for (int  i = 0; i < n; i++)
+    std::cin >> a[i];
+
+  std::map<int, int> m;
+  for (int  i = 0; i < n; i++)
+    m[a[i]]++; // m[key] = value;
+
+  for(auto it : m)
+    std::cout << it.first << " : " << it.second << std::endl;
+  return 0;
+}
